d551420d6e056d7df587bbb2c006fb0d905cf413
むずでょ <muzudho1@gmail.com>
2021-09-17 20:37:11 +0900

    ビュークラスの名前の末尾にViewって付けた

---
+++ WpfWindowPractice/WpfWindowPractice/MainWindowView.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace WpfWindowPractice
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindowView.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            var win = new Window1View();
+            win.Show();
+        }
+    }
+}
---
+++ WpfWindowPractice/WpfWindowPractice/Window1View.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WpfWindowPractice
+{
+    /// <summary>
+    /// Window1View.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Window1View : Window
+    {
+        public Window1View()
+        {
+            InitializeComponent();
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
